43e5513deb3d5ee9d3fe4344e21a5cf497a79c92
PraveenaKarthikeyan <praveenashrira21@gmail.com>
2025-03-12 00:11:27 +0530

    11th March- pushing to Git

---
+++ src/ex_01_Java_Basic/Lab005_MultipleTableWithLoop.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab005_MultipleTableWithLoop {
+    public static void main(String[] args) {
+        int a=9;
+        for(int b=1;b<=10;b++)
+        {
+            System.out.printf("%d * %d = %d\n",a,b,a*b);
+        }
+    }
+}
