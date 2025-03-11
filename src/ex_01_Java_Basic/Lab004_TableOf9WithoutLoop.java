bcee46d8e2ff489ddad813f32daf3d3a67c6cb97
PraveenaKarthikeyan <praveenashrira21@gmail.com>
2025-03-12 00:01:53 +0530

    11th March- pushing to Git

---
+++ src/ex_01_Java_Basic/Lab004_TableOf9WithoutLoop.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab004_TableOf9WithoutLoop {
+    public static void main(String[] args) {
+        int t=9;
+        System.out.printf("%d * %d = %d\n",t,1,t*1);
+        System.out.printf("%d * %d = %d\n",t,2,t*2);
+        System.out.printf("%d * %d = %d\n",t,3,t*3);
+        System.out.printf("%d * %d = %d\n",t,4,t*4);
+        System.out.printf("%d * %d = %d\n",t,5,t*5);
+        System.out.printf("%d * %d = %d\n",t,6,t*6);
+        System.out.printf("%d * %d = %d\n",t,7,t*7);
+        System.out.printf("%d * %d = %d\n",t,8,t*8);
+        System.out.printf("%d * %d = %d\n",t,9,t*9);
+        System.out.printf("%d * %d = %d\n",t,10,t*10);
+    }
+}
