9c6b5aceaa28a85d260e12195e2eaba1f8a67d47
PraveenaKarthikeyan <praveenashrira21@gmail.com>
2025-04-12 00:58:38 +0530

    12th April 2025-Pushing to Git

---
+++ src/dayWise_Tasks/JavaCoding/Lab27_CheckAlphabet.java
@@ -0,0 +1,20 @@
+package dayWise_Tasks.JavaCoding;
+
+import java.util.Scanner;
+
+public class Lab27_CheckAlphabet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0); // Read single character
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println(ch + " is an alphabet.");
+        } else {
+            System.out.println(ch + " is NOT an alphabet.");
+        }
+
+        scanner.close();
+    }
+}
