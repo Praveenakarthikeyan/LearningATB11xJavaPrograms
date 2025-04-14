9944416271db2818376952b160f101284d1cf618
PraveenaKarthikeyan <praveenashrira21@gmail.com>
2025-04-14 09:06:33 +0530

    14th April 2025-Pushing to Git

---
+++ src/JavaCodingQs/Q2_ConversionOfDays.java
@@ -0,0 +1,21 @@
+package JavaCodingQs;
+
+import java.util.Scanner;
+
+public class Q2_ConversionOfDays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the days:");
+        int totalDays = sc.nextInt();
+
+        int years=totalDays/365;
+        int remainingDays=totalDays%365;
+        int months=totalDays/30;
+        int days=totalDays%30;
+        System.out.println(years + " years, " + months + " months, and " + days + " days.");
+
+
+
+    }
+}
